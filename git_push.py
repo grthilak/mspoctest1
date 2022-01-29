48e4f56865bb6639e082cba33e38d9f3b0c53e61
grthilak <rajthilak.sitm@gmail.com>
2022-01-29 04:01:52 +0000

    updated report

---
+++ git_push.py
@@ -0,0 +1,15 @@
+import subprocess
+
+
+def execute_cmd(cmd):
+    process = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE, universal_newlines=True)
+    stdout,stderr = process.communicate()
+    print("output: {}".format(stdout))
+    print("error:".format(stderr))
+
+execute_cmd("df -h")
+execute_cmd("ls -l")
+
+execute_cmd("git add .")
+execute_cmd("git commit")
+#execute_cmd("")
